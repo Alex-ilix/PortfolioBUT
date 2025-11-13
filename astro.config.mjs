1f9c69930f45609c7122c12411952a2dd51b67bb
alexandre simongiovanni <simonlex34@gmail.com>
2025-11-13 17:22:31 +0100

    deployment2.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://alex-ilix.github.io/PortfolioBUT/',
-  base: '/PortfolioBUT/', // très important pour que les chemins des fichiers CSS/JS soient corrects
+  site: 'https://alex-ilix.github.io/PortfolioBUT/index.astro',
+  base: 'PortfolioBUT',
+  integrations: [tailwind(), mdx(), icon({
+    include: {
+      mdi: ["*"]
+    }
+  })],
+
 });
